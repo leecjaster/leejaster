75497812a23284f68681720b17070cb9ed9d5467
leecjaster <35693724+leecjaster@users.noreply.github.com>
2023-02-07 10:32:16 -0600

    updates

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'leecjaster1');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Akyint33#');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
